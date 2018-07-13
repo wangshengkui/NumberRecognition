61939067ff9241a1e18d49197576eb569703d002
woaiwyhty <398789568@qq.com>
2018-07-13 12:36:12 +0800

    Get image signature

---
+++ Train.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def calcDataSignature(filename):
+    result = [];
+    fs = open(filename);
+    for i in range(32):
+        line = fs.readline();
+        for j in range(32):
+            result.append(int(line[j]));
+    return np.array(result);
+
+print (calcDataProperty('data/digits/testDigits/0_1.txt').shape);
