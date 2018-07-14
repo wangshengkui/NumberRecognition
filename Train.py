a9e713ff5800bca3d9d7292f163ade5931eef11f
woaiwyhty <398789568@qq.com>
2018-07-14 11:50:00 +0800

    Get image signature

--- Train.py
+++ Train.py
@@ -10,4 +10,24 @@ def calcDataSignature(filename):
             result.append(int(line[j]));
     return np.array(result);
 
+def classify(currentFeature, featureSet, labels, K):
+    numsOfDatas = featureSet.shape[0];
+    curdataSet = np.repeat(currentFeature, numsOfDatas, axis = 0) - featureSet;
+    distance = curdataSet ** 2;
+    distanceSum = distance.sum(axis = 1); # calculate the eculid distance
+    distanceSum = distanceSum ** (0.5);
+    sortedIndex = distanceSum.argsort();
+    result = [];
+    weight = {};
+    for i in range(K): # get the most similar K data
+        weight[labels[sortedIndex[i]]] = weight.get(labels[sortedIndex[i]], 0) + 1;
+    curMax = 0;
+    ans = -1;
+    for key in weight:
+        if weight[key] > curMax:
+            curMax = weight[key];
+            ans = key;
+    return ans;
+
+
 print (calcDataSignature('data/digits/testDigits/0_1.txt').shape);
