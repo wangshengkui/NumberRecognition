5184b17ccd90532f7c9cce89b2c3fd9af60f4e0a
woaiwyhty <398789568@qq.com>
2018-07-16 11:19:39 +0800

    implement a painter to draw digits by mouse

---
+++ painter.py
@@ -0,0 +1,155 @@
+import pygame
+from pygame.locals import *
+import math
+
+
+class Brush:
+    def __init__(self, screen):
+        self.screen = screen
+        self.color = (0, 0, 0)
+        self.size = 1
+        self.drawing = False
+        self.last_pos = None
+        self.style = True
+        self.brush = pygame.image.load("images/images/brush.png").convert_alpha()
+        self.brush_now = self.brush.subsurface((0, 0), (1, 1))
+
+    def start_draw(self, pos):
+        self.drawing = True
+        self.last_pos = pos
+
+    def end_draw(self):
+        self.drawing = False
+
+    def set_brush_style(self, style):
+        print("* set brush style to", style)
+        self.style = style
+
+    def get_brush_style(self):
+        return self.style
+
+    def get_current_brush(self):
+        return self.brush_now
+
+    def set_size(self, size):
+        if size < 1:
+            size = 1
+        elif size > 32:
+            size = 32
+        print("* set brush size to", size)
+        self.size = size
+        self.brush_now = self.brush.subsurface((0, 0), (size * 2, size * 2))
+
+    def get_size(self):
+        return self.size
+
+    def set_color(self, color):
+        self.color = color
+        for i in range(self.brush.get_width()):
+            for j in range(self.brush.get_height()):
+                self.brush.set_at((i, j),
+                                  color + (self.brush.get_at((i, j)).a,))
+
+    def get_color(self):
+        return self.color
+
+    def draw(self, pos):
+        if self.drawing:
+            for p in self._get_points(pos):
+                if self.style:
+                    self.screen.blit(self.brush_now, p)
+                else:
+                    pygame.draw.circle(self.screen, self.color, p, self.size)
+            self.last_pos = pos
+
+    def _get_points(self, pos):
+        points = [(self.last_pos[0], self.last_pos[1])]
+        len_x = pos[0] - self.last_pos[0]
+        len_y = pos[1] - self.last_pos[1]
+        length = math.sqrt(len_x ** 2 + len_y ** 2)
+        step_x = len_x / length
+        step_y = len_y / length
+        for i in range(int(length)):
+            points.append((points[-1][0] + step_x, points[-1][1] + step_y))
+        points = map(lambda x: (int(0.5 + x[0]), int(0.5 + x[1])), points)
+        return list(set(points))
+
+class Menu:
+    def __init__(self, screen):
+        self.screen = screen
+        self.brush = None
+
+        # 两种笔刷的按钮图标
+        self.pens = [
+            pygame.image.load("images/images/eraser.png").convert_alpha(),
+            pygame.image.load("images/images/calculate.png").convert_alpha(),
+        ]
+        # 计算坐标，便于绘制
+        self.pens_rect = []
+        for (i, img) in enumerate(self.pens):
+            rect = pygame.Rect(10, 10 + i * 64, 64, 64)
+            self.pens_rect.append(rect)
+
+
+    def set_brush(self, brush):
+        self.brush = brush
+        # 绘制菜单栏
+
+    def draw(self):
+        for (i, img) in enumerate(self.pens):
+            self.screen.blit(img, self.pens_rect[i].topleft)
+
+
+        # 定义菜单按钮的点击响应
+
+    def click_button(self, pos):
+        # 笔刷
+        for (i, rect) in enumerate(self.pens_rect):
+            if rect.collidepoint(pos):
+                self.brush.set_brush_style(bool(i))
+                return True
+        # 笔刷大小
+
+
+        return False
+class Painter:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((320, 320))
+        pygame.display.set_caption("Painter")
+        self.clock = pygame.time.Clock()
+        self.brush = Brush(self.screen)
+        self.brush.set_brush_style(False);
+        #self.brush.set_size(10);
+        self.menu = Menu(self.screen)
+        self.menu.set_brush(self.brush)
+
+    def run(self):
+        self.screen.fill((255, 255, 255))
+        while True:
+            self.clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.screen.fill((255, 255, 255))
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.pos[0] <= 74 and self.menu.click_button(event.pos):
+                        pass
+                    else:
+                        self.brush.start_draw(event.pos)
+                elif event.type == MOUSEMOTION:
+                    self.brush.draw(event.pos)
+                elif event.type == MOUSEBUTTONUP:
+                    self.brush.end_draw()
+            self.menu.draw()
+            pygame.display.update()
+
+
+def main():
+    app = Painter()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
